05c61c39cf088458632aef397a86bc6c6fe55d19
1 <margoloko@mail.ru>
2024-03-11 20:50:42 +0700

    create app useres

--- store/users/models.py
+++ store/users/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """Кастомная модель пользователя."""
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["id"]
