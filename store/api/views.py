c0a6dd4f3fd504fb70a76335ec767ca4a1a7c25f
1 <margoloko@mail.ru>
2024-03-12 21:30:32 +0700

    feature: Настроены эндпоинты, вьюсеты и сериализаторы для категорий и продуктов.

--- store/api/views.py
+++ store/api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from products.models import Category, Product, SubCategory
+from rest_framework.mixins import ListModelMixin
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from .pagination import CustomPagination
+from .serializers import CategorySerializer, ProductSerializer, SubCategorySerializer
+
+
+class ListViewSet(ListModelMixin, GenericViewSet):
+    """
+    Базовый класс вьюсета.
+    Возвращает список объектов (для обработки запросов GET).
+    """
+
+    pagination_class = CustomPagination
+
+
+class CategoryViewSet(ListViewSet):
+    """Представление для модели Category."""
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class SubCategoryViewSet(ListViewSet):
+    """Представление для модели SubCategory."""
+
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
+
+
+class ProductViewSet(ListViewSet):
+    """Представление для модели Product."""
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
