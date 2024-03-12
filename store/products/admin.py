308c3545acd679b3c1affb8d31e4e8377c842a6f
1 <margoloko@mail.ru>
2024-03-12 13:50:13 +0700

    create app products

--- store/products/admin.py
+++ store/products/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, SubCategory, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Админка для модели Category."""
+    
+    list_display = ("name", "slug",)
+    list_filter = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    """Админка для модели SubCategory."""
+    
+    list_display = ("name", "slug", "category")
+    list_filter = ("name", "category")
+    search_fields = ("name", "category")
+    ordering = ("name", "category")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Админка для модели Product."""
+    
+    list_display = ("name", "slug", "price", "subcategory")
+    list_filter = ("name", "price", "subcategory")
+    search_fields = ("name", "price", "subcategory")
+    ordering = ("name", "price", "subcategory",)
--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+from users.models import CustomUser as User
+
+
+class Category(models.Model):
+    """Модель категории товаров."""
+
+    name = models.CharField(max_length=150,
+                            verbose_name='Наименование категории')
+    slug = models.SlugField(unique=True,
+                            verbose_name='Slug')
+    image = models.ImageField(upload_to='media/categories/',
+                              help_text="Изображение категории")
+    
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    """Модель подкатегории товаров."""
+
+    name = models.CharField(max_length=150,
+                            verbose_name='Наименование подкатегории')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='media/subcategories/',
+                              verbose_name="Изображение подкатегории")
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "Подкатегория"
+        verbose_name_plural = "Подкатегории"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """Модель продуктов."""
+
+    name = models.CharField(max_length=200,
+                            verbose_name="Наименование продукта")
+    slug = models.SlugField(unique=True)
+    image_small = models.ImageField(upload_to='media/products/small/',
+                                    verbose_name="Маленькое изображение продукта",
+                                    help_text="Изображение продукта в маленьком размере")
+    image_medium = models.ImageField(upload_to='media/products/medium/',
+                                     verbose_name="Среднее изображение",
+                                     help_text="Изображение продукта в среднем размере")
+    image_large = models.ImageField(upload_to='media/products/large/',
+                                    verbose_name="Большое изображение",
+                                    help_text="Изображение продукта в большом размере")
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2,
+                                verbose_name="Цена")
+    subcategory = models.ForeignKey(SubCategory,
+                                    on_delete=models.CASCADE,
+                                    verbose_name="Подкатегория продукта")
+    
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
