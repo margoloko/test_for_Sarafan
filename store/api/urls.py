588b533afa89edbf8113d33dc328224c273d488d
1 <margoloko@mail.ru>
2024-03-14 17:07:40 +0700

    feature: Реализованы эндпоинты добавления, изменения (изменение количества), удаления продукта в корзине.

--- store/api/urls.py
+++ store/api/urls.py
@@ -1,14 +1,22 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, ProductViewSet, ShoppingCartViewSet
+from .views import (
+    AddItemsView,
+    CategoryViewSet,
+    ProductViewSet,
+    ShoppingCartItemsViewSet,
+    ShoppingCartViewSet,
+)
 
 router = DefaultRouter()
 
 router.register(r"categories", CategoryViewSet)
 router.register(r"products", ProductViewSet, basename="product")
 router.register(r"cart", ShoppingCartViewSet, basename="cart")
+router.register(r"cart/items", ShoppingCartItemsViewSet, basename="cartitem")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("shopping_cart/add/", AddItemsView.as_view(), name="add"),
 ]
